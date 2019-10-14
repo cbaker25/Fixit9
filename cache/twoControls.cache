77cdb467efcfb23489c38b3dfa127a90c3f001d1
Conor Baker <cab24313@creighton.edu>
2019-10-13 18:06:14 -0700

    done

---
+++ cache/twoControls.cache
@@ -0,0 +1,11 @@
+/*
+radio button changes- changed the background, values, and font
+
+*/
+Radiobutton1.onclick=function(){
+  NSB.MsgBox("Great Choice!")
+}
+
+twoControls.onkeypress=function(){
+  NSB.MsgBox("You have 50% of the quiz left.")
+}
---
+++ forms/Calculator/Calculator.js
@@ -0,0 +1,12 @@
+n1 = firstNumber.value
+n2= secondNumber.value
+
+Compute.onclick=function addTwo(n1, n2){
+ return (n1 + n2)
+ lblAnswer = addTwo(inptNum1.value,inptNum2.value)
+}
+
+Reset.onclick=function(){
+  if (n1 = " ") && (n2 = " ")
+  firstNumber.clear()
+}
